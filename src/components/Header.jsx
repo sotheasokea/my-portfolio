59bf2357254a59a76caa7fa150de5c1963388ab6
sotheasokea <sotheasokea7@gmail.com>
2026-06-23 12:57:34 +0700

    move code Header and HeroSecotion to different file

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export function Header() {
+  return (
+    <>
+      <header class="top-header">
+        <button class="hamburger-menu" aria-label="Open menu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <h1 class="site-title">Welcome to my website</h1>
+      </header>
+    </>
+  );
+}
