dd39a1ef57529c4a1b42b8eee9bb03aa89c9e41a
sotheasokea <sotheasokea7@gmail.com>
2026-06-26 22:37:59 +0700

    create aboutData for the experiencs

--- src/sections/about/AboutItem.jsx
+++ src/sections/about/AboutItem.jsx
@@ -1,7 +1,5 @@
-import profileIcon from '../../assets/images/profile-icon.png'
-import mortarboardIcon from '../../assets/images/mortarboard-icon.png'
-import projectIcon from '../../assets/images/project-icon.png'
-import internshipIcon from '../../assets/images/data-entry-internship-icon.png'
+import profileIcon from "../../assets/images/profile-icon.png";
+import { experiences } from "./aboutData";
 export function AboutItem() {
   return (
     <>
@@ -16,40 +14,22 @@ export function AboutItem() {
         </p>
       </div>
 
-      <div className="about-item">
-        <img src={mortarboardIcon} alt="Education icon" />
-      </div>
-      <div className="about-entry">
-        <h3>Bachelor of Computer Science</h3>
-        <span className="date">2024 - present</span>
-        <p>
-          Currently pursuing my degree with a focus on software developing,
-          machine learning and AI.
-        </p>
-      </div>
+      {experiences.map((experience) => {
+        return (
+          <>
+            <div className="about-item">
+              <img src={experience.icon} alt="Education icon" />
+            </div>
+            <div className="about-entry">
+              <h3>{experience.title}</h3>
+              <span className="date">{experience.date}</span>
+              <p>{experience.description}</p>
+            </div>
+          </>
+        );
+      })}
 
-      <div className="about-item">
-        <img src={projectIcon} alt="Projects icon" />
-      </div>
-      <div className="about-entry">
-        <h3>Personal Projects</h3>
-        <span className="date">2024 - present</span>
-        <p>
-          Building projects to strengthen my skills and solve real world
-          problems.
-        </p>
-      </div>
-
-      <div className="about-item">
-        <img src={internshipIcon} alt="Internship icon" />
-      </div>
-      <div className="about-entry">
-        <h3>Data Entry Internship</h3>
-        <span className="date">2026</span>
-        <p>
-          Finding scholarship data at a startup and upload them to the database.
-        </p>
-      </div>
+      
     </>
   );
 }
