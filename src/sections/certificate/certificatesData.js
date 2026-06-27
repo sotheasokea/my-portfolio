638e51d544c6bce07d951b9d633ab06bf0b6df01
sotheasokea <sotheasokea7@gmail.com>
2026-06-27 21:53:18 +0700

    add data for certificate sections

--- src/sections/certificate/certificatesData.js
+++ src/sections/certificate/certificatesData.js
@@ -1,5 +1,9 @@
-import googleIcon from '../../assets/certificate/google-icon.png'
+// import googleIcon from '../../assets/certificate/google-icon.png'
 import aiClassAsean from '../../assets/certificate/ai-class-asean.png'
+import hackerRankProfile from '../../assets/certificate/hacker-rank-logo.png'
+import ciscoIcon from '../../assets/certificate/cisco-icon.jpg'
+import speexxIcon from '../../assets/certificate/speexx-icon.png'
+import thaiMoocIcon from '../../assets/certificate/thai-mooc-icon.png'
 export const certifcations = [
   {
     id:  1, 
@@ -11,34 +15,42 @@ export const certifcations = [
   },
   {
     id:  2, 
-    icon: googleIcon,
-    course: "Google Cybersecurity Professional Certificate",
-    provider: "Google",
-    date: "March 2023",
-    certificateLink: "/certificates/ai-class-asean-sothea.pdf"
+    icon: hackerRankProfile,
+    course: "HackerRank Certificates",
+    provider: "HackerRank",
+    date: "March 13 2025",
+    certificateLink: "https://www.hackerrank.com/profile/sokeasothea"
   },
   {
     id:  3, 
-    icon: googleIcon,
-    course: "Google Cybersecurity Professional Certificate",
-    provider: "Google",
-    date: "March 2023",
-    certificateLink: "/certificates/ai-class-asean-sothea.pdf"
+    icon: ciscoIcon,
+    course: "Networking Basics",
+    provider: "Cisco Networking Academy",
+    date: "March 29 2026",
+    certificateLink: "/certificates/networking-basics.pdf"
   },
   {
     id:  4, 
-    icon: googleIcon,
-    course: "Google Cybersecurity Professional Certificate",
-    provider: "Google",
-    date: "March 2023",
-    certificateLink: "/certificates/ai-class-asean-sothea.pdf"
+    icon: ciscoIcon,
+    course: "Computer Hardware Basics",
+    provider: "Cisco Networking Academy",
+    date: "December 17 2025",
+    certificateLink: "/certificates/computer-hardware-basics-ceritficate.pdf"
+  },
+  {
+    id:  6, 
+    icon: speexxIcon,
+    course: "English B2 Test",
+    provider: "speexx",
+    date: "January 8 2026",
+    certificateLink: "/certificates/speexx-certificate.pdf"
   },
   {
     id:  5, 
-    icon: googleIcon,
-    course: "Google Cybersecurity Professional Certificate",
-    provider: "Google",
-    date: "March 2023",
-    certificateLink: "/certificates/ai-class-asean-sothea.pdf"
+    icon: thaiMoocIcon,
+    course: "Basic web page with HTML and CSS",
+    provider: "Chulalongkorn University",
+    date: "December 15 2025",
+    certificateLink: "/certificates/basic-web-page-with-html-and-css.pdf"
   }
 ];
